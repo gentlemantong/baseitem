171a4de4d326af0784732496f4b460e97b8f6fdc
tongxiao <tongxiao@yeah.net>
2018-01-14 14:47:05 +0800

    Update baseitem.py
    change imports

--- baseitem.py
+++ baseitem.py
@@ -2,9 +2,13 @@
 import logging
 import re
 import sys
+import weakref
+from abc import ABCMeta
+from collections import MutableMapping, defaultdict
+from pprint import pformat
+from time import time
 
 import six
-from scrapy.item import DictItem, ItemMeta, Field
 
 _child_allowed_types = [list]
 
@@ -13,8 +17,134 @@ if sys.version_info < (3, 4):
 else:
     _char_types = [str]
 
+live_refs = defaultdict(weakref.WeakKeyDictionary)
 
-class Type(Field):
+
+class ItemMeta(ABCMeta):
+
+    def __new__(mcs, class_name, bases, attrs):
+        classcell = attrs.pop('__classcell__', None)
+        new_bases = tuple(base._class for base in bases if hasattr(base, '_class'))
+        _class = super(ItemMeta, mcs).__new__(mcs, 'x_' + class_name, new_bases, attrs)
+
+        fields = getattr(_class, 'fields', {})
+        new_attrs = {}
+        for n in dir(_class):
+            v = getattr(_class, n)
+            if isinstance(v, dict):
+                fields[n] = v
+            elif n in attrs:
+                new_attrs[n] = attrs[n]
+
+        new_attrs['fields'] = fields
+        new_attrs['_class'] = _class
+        if classcell is not None:
+            new_attrs['__classcell__'] = classcell
+        return super(ItemMeta, mcs).__new__(mcs, class_name, bases, new_attrs)
+
+
+class ObjectRef(object):
+    """
+    从这个类（而不是Object）继承到保存实例的记录
+    """
+
+    __slots__ = ()
+
+    def __new__(cls, *args, **kwargs):
+        obj = object.__new__(cls)
+        live_refs[cls][obj] = time()
+        return obj
+
+
+class DictItem(MutableMapping, ObjectRef):
+
+    fields = {}
+
+    def __init__(self, *args, **kwargs):
+        self._values = {}
+        if args or kwargs:  # avoid creating dict for most common case
+            for k, v in six.iteritems(dict(*args, **kwargs)):
+                self[k] = v
+
+    def __getitem__(self, key):
+        return self._values[key]
+
+    def __setitem__(self, key, value):
+        if key in self.fields:
+            data_type = self.fields[key][u'data_type']
+            if data_type in _child_allowed_types:
+                child_type = self.fields[key][u'child_type']
+                val_list = []
+                for val in value:
+                    try:
+                        if (child_type in _char_types and type(val) in _char_types) or isinstance(val, child_type):
+                            val_list.append(val)
+                        else:
+                            raise TypeError(u"({0}, {1}) is not a valid child-record for '{2}' while whose standard"
+                                            u" child_type is {3}".format(val, type(val), key, child_type))
+                    except Exception as e:
+                        logging.exception(u"Invalid child-record, it'll be passed! - {0}".format(e))
+                self._values[key] = val_list
+            elif (data_type in _char_types and type(value) in _char_types) or isinstance(value, data_type):
+                self._values[key] = value
+            else:
+                raise TypeError(u"({0}, {1}) is not valid for '{2}' while whose standard type is {3}".format(
+                    value, type(value), key, data_type))
+        else:
+            raise KeyError("%s does not support field: %s" % (self.__class__.__name__, key))
+
+    def __delitem__(self, key):
+        del self._values[key]
+
+    def __getattr__(self, name):
+        return self._values[name]
+
+    def __setattr__(self, name, value):
+        if not name.startswith(u'_'):
+            if name in self.fields:
+                data_type = self.fields[name][u'data_type']
+                if data_type in _child_allowed_types:
+                    child_type = self.fields[name][u'child_type']
+                    val_list = []
+                    for val in value:
+                        try:
+                            if (child_type in _char_types and type(val) in _char_types) or isinstance(val, child_type):
+                                val_list.append(val)
+                            else:
+                                raise TypeError(u"({0}, {1}) is not a valid child-record for '{2}' while whose standard"
+                                                u" child_type is {3}".format(val, type(val), name, child_type))
+                        except Exception as e:
+                            logging.exception(u"Invalid child-record, it'll be passed! - {0}".format(e))
+                    self._values[name] = val_list
+                elif (data_type in _char_types and type(value) in _char_types) or isinstance(value, data_type):
+                    self._values[name] = value
+                else:
+                    raise TypeError(u"({0}, {1}) is not valid for '{2}' while whose standard type is {3}".format(
+                        value, type(value), name, data_type))
+            else:
+                raise KeyError("%s does not support field: %s" % (self.__class__.__name__, name))
+        else:
+            super(DictItem, self).__setattr__(name, value)
+
+    def __len__(self):
+        return len(self._values)
+
+    def __iter__(self):
+        return iter(self._values)
+
+    __hash__ = ObjectRef.__hash__
+
+    def keys(self):
+        return self._values.keys()
+
+    def __repr__(self):
+        return pformat(dict(self))
+
+    def copy(self):
+        return self.__class__(self)
+
+
+class Type(dict):
     """字段数据类型类"""
 
     def __init__(self, data_type=None, child_type=None):
@@ -51,50 +181,6 @@ class BaseItem(DictItem):
         """
         super(BaseItem, self).__init__(*args, **kwargs)
 
-    def __getattr__(self, name):
-        """
-        重写属性获取方法
-        :param name: 属性名
-        :return: 属性值
-        """
-        if name in self.fields:
-            return self._values[name]
-        else:
-            raise AttributeError(u'Invalid field: {0}!'.format(name))
-
-    def __setattr__(self, name, value):
-        """
-        重写属性赋值方法
-        :param name: 属性名
-        :param value: 属性值
-        :return: 无
-        """
-        if not name.startswith(u'_'):
-            if name in self.fields:
-                data_type = self.fields[name][u'data_type']
-                if data_type in _child_allowed_types:
-                    child_type = self.fields[name][u'child_type']
-                    val_list = []
-                    for val in value:
-                        try:
-                            if (child_type in _char_types and type(val) in _char_types) or isinstance(val, child_type):
-                                val_list.append(val)
-                            else:
-                                raise TypeError(u"({0}, {1}) is not a valid child-record for '{2}' while whose standard"
-                                                u" child_type is {3}".format(val, type(val), name, child_type))
-                        except Exception as e:
-                            logging.exception(u"Invalid child-record, it'll be passed! - {0}".format(e))
-                    self._values[name] = val_list
-                elif (data_type in _char_types and type(value) in _char_types) or isinstance(value, data_type):
-                    self._values[name] = value
-                else:
-                    raise TypeError(u"({0}, {1}) is not valid for '{2}' while whose standard type is {3}".format(
-                        value, type(value), name, data_type))
-            else:
-                raise AttributeError(u'Invalid field: {0}!'.format(name))
-        else:
-            super(BaseItem, self).__setattr__(name, value)
-
 
 def dumps_item(item):
     """
@@ -124,13 +210,13 @@ def dumps_item(item):
                 values = getattr(item, key)
                 for val in values:
                     if issubclass(child_type, BaseItem):
-                        val_obj = dumps_item_to_dict(val)
+                        val_obj = dumps_item(val)
                     else:
                         val_obj = getattr(item, key)
                     val_list.append(val_obj)
                 result_dict[key] = val_list
             elif issubclass(data_type, BaseItem):
-                result_dict[key] = dumps_item_to_dict(getattr(item, key))
+                result_dict[key] = dumps_item(getattr(item, key))
             else:
                 result_dict[key] = getattr(item, key)
     return result_dict
