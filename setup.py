6504fca009e85c488278f5c0a2bfc455fde1a562
tongxiao <tongxiao@yeah.net>
2018-01-14 14:56:30 +0800

    Update setup.py
    change the version and references

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name=u'baseitem',
-    version=u'1.1',
+    version=u'1.2',
     description=u'A simple library for converting complex data-types to and from native Python data-types.',
     keywords=u'python convert data-types',
     author=u'tongxiao',
@@ -12,6 +12,6 @@ setup(
     license=u'MIT',
     author_email=u'tongxiao@yeah.net',
     url=u'https://github.com/gentlemantong/baseitem',
-    install_requires=[u'scrapy'],
+    install_requires=[u'six'],
     py_modules=[u'baseitem']
 )
