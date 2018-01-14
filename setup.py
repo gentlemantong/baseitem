3f2b3431971355485ff6894a835bad0e065a4f05
tongxiao <tongxiao@yeah.net>
2018-01-14 15:57:27 +0800

    Update setup.py
    update the version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name=u'baseitem',
-    version=u'1.2',
+    version=u'1.4',
     description=u'A simple library for converting complex data-types to and from native Python data-types.',
     keywords=u'python convert data-types',
     author=u'tongxiao',
